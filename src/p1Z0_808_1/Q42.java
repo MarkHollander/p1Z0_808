4e93dd154c568edbf1d77b57a967d5573f9104b2
MarkHollander <minhvudoi3@gmail.com>
2024-01-16 11:17:31 +0700

    Q42

---
+++ src/p1Z0_808_1/Q42.java
@@ -0,0 +1,8 @@
+package p1Z0_808_1;
+
+public class Q42 {
+    public static void main(String[] args) {
+        System.out.println("5+2=" + 3 + 4);
+        System.out.println("5+2=" + (3 + 4));
+    }
+}
