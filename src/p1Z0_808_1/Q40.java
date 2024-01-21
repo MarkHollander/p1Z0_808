22021bac13d2b6b47eee70da3ab1150237e3e641
MarkHollander <minhvudoi3@gmail.com>
2024-01-21 09:19:28 +0700

    Q40

--- src/p1Z0_808_1/Q40.java
+++ src/p1Z0_808_1/Q40.java
@@ -6,23 +6,23 @@ public class Q40 extends DerivedA {
     }
 
     public static void main(String[] args) {
-        // Base b1 = new Q40();
-        // Base b2 = new DerivedA();
-        // Base b3 = new Q40();
-        // b1 = (Base) b3;
-        // Base b4 = (DerivedA) b3;
-        // b1.test();
-        // b4.test();
+        Base b1 = new Q40();
+        Base b2 = new DerivedA();
+        Base b3 = new Q40();
+        b1 = (Base) b3;
+        Base b4 = (DerivedA) b3;
+        b1.test();
+        b4.test();
 
 
-        Q40 t1 = new Q40();
-        Q40 t2 = new Q40();
-        if (!t1.equals(t2)) {
-            System.out.println("They are not equal");
-        } ;
-        if (t1 instanceof Object) {
-            System.out.println("t1 is Object");
-        } ;
+        // Q40 t1 = new Q40();
+        // Q40 t2 = new Q40();
+        // if (!t1.equals(t2)) {
+        //     System.out.println("They are not equal");
+        // } ;
+        // if (t1 instanceof Object) {
+        //     System.out.println("t1 is Object");
+        // } ;
     }
     
 }
