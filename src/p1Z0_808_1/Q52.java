8ddbf995bc121532c2d6cf8c4620997d42d749a3
MarkHollander <minhvudoi3@gmail.com>
2024-01-27 22:00:53 +0700

    Q52_1

---
+++ src/p1Z0_808_1/Q52.java
@@ -0,0 +1,15 @@
+package p1Z0_808_1;
+
+public class Q52 {
+
+    public static void main(int[] args) {
+        System.out.println("int main" + args[0]);
+    }
+    // public static void main(String[] args) {
+    //     System.out.println("String main" + args[0]);
+    // }
+
+    // public static void main(Object[] args) {
+    //     System.out.println("Object main" + args[0]);
+    // }
+}
