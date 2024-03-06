19bcf5b715a322a88a926523e76290e79162ddc0
MarkHollander <minhvudoi3@gmail.com>
2024-03-06 09:53:50 +0700

    Q93_2-Q99_2

---
+++ src/p1Z0_808_2/Q99.java
@@ -0,0 +1,13 @@
+package p1Z0_808_2;
+
+public class Q99 {
+    public static void main(String[] args) {
+        int numbers[] = {12, 13, 42, 32, 15, 156, 23, 51, 12};
+        int max = findMax(numbers);
+    }
+
+    static int findMax(int[] numbers) {
+        int max = 0;
+        return max;
+    }
+}
